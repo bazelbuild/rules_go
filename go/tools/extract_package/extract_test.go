962a848fabe61687d404d602d4bf564e7515f288
Yuki Yugui Sonoda <yugui@yugui.jp>
2016-04-11 18:53:42 +0900

    Avoid depending on the working directory in the test

--- go/tools/extract_package/extract_test.go
+++ go/tools/extract_package/extract_test.go
@@ -1,21 +1,45 @@
 package main
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
 func TestExtract(t *testing.T) {
-	for _, fname := range []string{
-		"extract.go",
-		"extract_test.go",
-		"ignored.go",
+	for _, spec := range []struct {
+		src  string
+		want string
+	}{
+		// These example inputs also illustrate the reason why we
+		// cannot simply replace extract_package with sed(1) or awk(1).
+		{
+			src:  `package main`,
+			want: "main",
+		},
+		{
+			src:  `package /* package another */ example // package yetanother`,
+			want: "example",
+		},
 	} {
-		name, err := extract(fname)
+		f, err := ioutil.TempFile(os.Getenv("TEST_TMPDIR"), "example-go-src")
 		if err != nil {
-			t.Errorf("extract(%q) fails with %v; want success", fname, err)
+			t.Fatal(err)
 		}
-		if got, want := name, "main"; got != want {
-			t.Errorf("extract(%q) = %q; want %q", fname, got, want)
+		defer os.Remove(f.Name())
+		if err := f.Close(); err != nil {
+			t.Fatal(err)
+		}
+		if err := ioutil.WriteFile(f.Name(), []byte(spec.src), 0644); err != nil {
+			t.Fatal(err)
+		}
+
+		name, err := extract(f.Name())
+		if err != nil {
+			t.Errorf("extract(%q) failed with %v; want success; content = %q", f.Name(), err, spec.src)
+		}
+		if got, want := name, spec.want; got != want {
+			t.Errorf("extract(%q) = %q; want %q; content = %q", f.Name(), got, want, spec.src)
 		}
 	}
 }
--- go/tools/extract_package/ignored.go
+++
@@ -1,3 +0,0 @@
-// +build ignored
-
-package main
