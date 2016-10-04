bazelbuild/rules_go

commit 22ababf573115a40fbfc79b83f264668185f2d42

Apply the given copts to cgo code generation (#123)

examples/cgo/BUILD
@@ -15,6 +15,7 @@ cgo_library(
     ],
     cdeps = ["//examples/cgo/cc_dependency:version"],
     clinkopts = ["-lm"],
+    copts = ["-DDEFINED_IN_COPTS=\\\"Don't panic\\\""],
     visibility = ["//visibility:private"],
     deps = [":sub"],
 )

examples/cgo/import_example.go
@@ -23,3 +23,7 @@ func PrintGoVersion() {
 func printCXXVersion() {
 	C.PrintCXXVersion()
 }
+
+func ReturnDefined() string {
+	return C.DEFINED_IN_COPTS
+}

go/def.bzl
@@ -684,7 +684,7 @@ _cgo_codegn_rule = rule(
     output_to_genfiles = True,
 )
 
-def _cgo_codegen(name, srcs, c_hdrs=[], deps=[], linkopts=[],
+def _cgo_codegen(name, srcs, c_hdrs=[], deps=[], copts=[], linkopts=[],
                  go_tool=None, toolchain=None):
   """Generates glue codes for interop between C and Go
 
@@ -733,6 +733,7 @@ def _cgo_codegen(name, srcs, c_hdrs=[], deps=[], linkopts=[],
       srcs = srcs,
       c_hdrs = c_hdrs,
       deps = deps,
+      copts = copts,
       linkopts = linkopts,
 
       go_tool = go_tool,
@@ -909,6 +910,7 @@ def cgo_library(name, srcs,
       srcs = go_srcs,
       c_hdrs = c_hdrs,
       deps = cdeps,
+      copts = copts,
       linkopts = clinkopts,
       go_tool = go_tool,
       toolchain = toolchain,

commit ff68eec0beb026ec471ac0017413da070e66184f

Fixes a build error introduced in #123

examples/cgo/BUILD
@@ -15,7 +15,7 @@ cgo_library(
     ],
     cdeps = ["//examples/cgo/cc_dependency:version"],
     clinkopts = ["-lm"],
-    copts = ["-DDEFINED_IN_COPTS=\\\"Don't panic\\\""],
+    copts = ["-DDEFINED_IN_COPTS=1"],
     visibility = ["//visibility:private"],
     deps = [":sub"],
 )

examples/cgo/import_example.go
@@ -24,6 +24,6 @@ func printCXXVersion() {
 	C.PrintCXXVersion()
 }
 
-func ReturnDefined() string {
-	return C.DEFINED_IN_COPTS
+func ReturnDefined() int {
+	return int(C.DEFINED_IN_COPTS)
 }