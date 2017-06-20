bazelbuild/rules_go

commit cafe930223abe6f5faa0561d40435b52fbea77b9

Gazelle: refactor configuration plumbing (#553)
* Added a new "config" package with a "Config" struct that contains
  most of the configuration information that Gazelle gets from flags
  (go prefix, repo root, etc.). Defaults and utility methods are also
  moved to this package.
* Added newConfiguration, a function which builds this
  configuration. Tags are preprocessed, and paths are canonicalized so
  we don't have to worry about that later.
* Functions which previously accepted arguments that are found in
  Config now just accept *config.Config.
* Deleted packageReader, since it had a very similar purpose.

go/tools/gazelle/config/config.go
@@ -1,0 +1,128 @@
+/* Copyright 2017 The Bazel Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package config
+
+import (
+	"fmt"
+	"go/build"
+)
+
+// Config holds information about how Gazelle should run. This is mostly
+// based on command-line arguments.
+type Config struct {
+	// Dirs is a list of absolute paths to directories where Gazelle should run.
+	Dirs []string
+
+	// RepoRoot is the absolute path to the root directory of the repository.
+	RepoRoot string
+
+	// ValidBuildFileNames is a list of base names that are considered valid
+	// build files. Some repositories may have files named "BUILD" that are not
+	// used by Bazel and should be ignored. Must contain at least one string.
+	ValidBuildFileNames []string
+
+	// GenericTags is a set of build constraints that are true on all platforms.
+	// It should not be nil.
+	GenericTags BuildTags
+
+	// Platforms contains a set of build constraints for each platform. Each set
+	// should include GenericTags. It should not be nil.
+	Platforms PlatformTags
+
+	// GoPrefix is the portion of the import path for the root of this repository.
+	// This is used to map imports to labels within the repository.
+	GoPrefix string
+
+	// DepMode determines how imports outside of GoPrefix are resolved.
+	DepMode DependencyMode
+}
+
+var DefaultValidBuildFileNames = []string{"BUILD.bazel", "BUILD"}
+
+func (c *Config) IsValidBuildFileName(name string) bool {
+	for _, n := range c.ValidBuildFileNames {
+		if name == n {
+			return true
+		}
+	}
+	return false
+}
+
+func (c *Config) DefaultBuildFileName() string {
+	return c.ValidBuildFileNames[0]
+}
+
+// BuildTags is a set of build constraints.
+type BuildTags map[string]bool
+
+// PlatformTags is a map from config_setting labels (for example,
+// "@io_bazel_rules_go//go/platform:linux_amd64") to a sets of build tags
+// that are true on each platform (for example, "linux,amd64").
+type PlatformTags map[string]BuildTags
+
+// DefaultPlatformTags is the default set of platforms that Gazelle
+// will generate files for. These are the platforms that both Go and Bazel
+// support.
+var DefaultPlatformTags PlatformTags
+
+func init() {
+	DefaultPlatformTags = make(PlatformTags)
+	arch := "amd64"
+	for _, os := range []string{"darwin", "linux", "windows"} {
+		label := fmt.Sprintf("@io_bazel_rules_go//go/platform:%s_%s", os, arch)
+		DefaultPlatformTags[label] = BuildTags{arch: true, os: true}
+	}
+}
+
+// PreprocessTags performs some automatic processing on generic and
+// platform-specific tags before they are used to match files.
+func (c *Config) PreprocessTags() {
+	c.GenericTags["cgo"] = true
+	c.GenericTags["gc"] = true
+	for _, t := range build.Default.ReleaseTags {
+		c.GenericTags[t] = true
+	}
+	for _, platformTags := range c.Platforms {
+		for t, _ := range c.GenericTags {
+			platformTags[t] = true
+		}
+	}
+}
+
+// DependencyMode determines how imports of packages outside of the prefix
+// are resolved.
+type DependencyMode int
+
+const (
+	// ExternalMode indicates imports should be resolved to external dependencies
+	// (declared in WORKSPACE).
+	ExternalMode DependencyMode = iota
+
+	// VendorMode indicates imports should be resolved to libraries in the
+	// vendor directory.
+	VendorMode
+)
+
+func DependencyModeFromString(s string) (DependencyMode, error) {
+	switch s {
+	case "external":
+		return ExternalMode, nil
+	case "vendor":
+		return VendorMode, nil
+	default:
+		return 0, fmt.Errorf("invalid dependency mode: %s", s)
+	}
+}

go/tools/gazelle/config/BUILD
@@ -1,0 +1,13 @@
+load("@io_bazel_rules_go//go:def.bzl", "go_library", "go_test")
+
+go_library(
+    name = "go_default_library",
+    srcs = ["config.go"],
+    visibility = ["//visibility:public"],
+)
+
+go_test(
+    name = "go_default_test",
+    srcs = ["config_test.go"],
+    library = ":go_default_library",
+)

go/tools/gazelle/config/config_test.go
@@ -1,0 +1,37 @@
+/* Copyright 2017 The Bazel Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package config
+
+import "testing"
+
+func TestPreprocessTags(t *testing.T) {
+	c := &Config{
+		GenericTags: map[string]bool{"a": true, "b": true},
+		Platforms:   DefaultPlatformTags,
+	}
+	c.PreprocessTags()
+	expectedTags := []string{"a", "b", "cgo", "gc", "go1.8", "go1.7"}
+	for _, tag := range expectedTags {
+		if !c.GenericTags[tag] {
+			t.Errorf("tag %q not set", tag)
+		}
+		for name, platformTags := range c.Platforms {
+			if !platformTags[tag] {
+				t.Errorf("on platform %q, tag %q not set", name, tag)
+			}
+		}
+	}
+}

go/tools/gazelle/gazelle/diff.go
@@ -21,10 +21,11 @@ import (
 	"os/exec"
 
 	bzl "github.com/bazelbuild/buildtools/build"
+	"github.com/bazelbuild/rules_go/go/tools/gazelle/config"
 )
 
-func diffFile(file *bzl.File) error {
-	f, err := ioutil.TempFile("", getBuildFileName())
+func diffFile(c *config.Config, file *bzl.File) error {
+	f, err := ioutil.TempFile("", c.DefaultBuildFileName())
 	if err != nil {
 		return err
 	}

go/tools/gazelle/gazelle/fix.go
@@ -19,9 +19,10 @@ import (
 	"io/ioutil"
 
 	bzl "github.com/bazelbuild/buildtools/build"
+	"github.com/bazelbuild/rules_go/go/tools/gazelle/config"
 )
 
-func fixFile(file *bzl.File) error {
+func fixFile(c *config.Config, file *bzl.File) error {
 	if err := ioutil.WriteFile(file.Path, bzl.Format(file), 0644); err != nil {
 		return err
 	}

go/tools/gazelle/gazelle/fix_test.go
@@ -23,13 +23,25 @@ import (
 	"testing"
 
 	bzl "github.com/bazelbuild/buildtools/build"
-	"github.com/bazelbuild/rules_go/go/tools/gazelle/rules"
+	"github.com/bazelbuild/rules_go/go/tools/gazelle/config"
 )
 
 func TestMain(m *testing.M) {
 	tmpdir := os.Getenv("TEST_TMPDIR")
 	flag.Set("repo_root", tmpdir)
 	os.Exit(m.Run())
+}
+
+func defaultConfig(dir string) *config.Config {
+	c := &config.Config{
+		Dirs:                []string{dir},
+		RepoRoot:            dir,
+		GenericTags:         config.BuildTags{},
+		Platforms:           config.DefaultPlatformTags,
+		ValidBuildFileNames: config.DefaultValidBuildFileNames,
+	}
+	c.PreprocessTags()
+	return c
 }
 
 func TestFixFile(t *testing.T) {
@@ -56,7 +68,8 @@ func TestFixFile(t *testing.T) {
 		},
 	}
 
-	if err := fixFile(stubFile); err != nil {
+	c := defaultConfig(dir)
+	if err := fixFile(c, stubFile); err != nil {
 		t.Errorf("fixFile(%#v) failed with %v; want success", stubFile, err)
 		return
 	}
@@ -86,7 +99,8 @@ func TestCreateFile(t *testing.T) {
 	}
 
 	// Check that Gazelle creates a new file named "BUILD.bazel".
-	run([]string{dir}, nil, fixFile, rules.External)
+	c := defaultConfig(dir)
+	run(c, fixFile)
 
 	buildFile := filepath.Join(dir, "BUILD.bazel")
 	if _, err = os.Stat(buildFile); err != nil {
@@ -114,7 +128,8 @@ func TestUpdateFile(t *testing.T) {
 	}
 
 	// Check that Gazelle updates the BUILD file in place.
-	run([]string{dir}, nil, fixFile, rules.External)
+	c := defaultConfig(dir)
+	run(c, fixFile)
 	if st, err := os.Stat(buildFile); err != nil {
 		t.Errorf("could not stat BUILD: %v", err)
 	} else if st.Size() == 0 {

go/tools/gazelle/gazelle/main.go
@@ -28,9 +28,9 @@ import (
 	"strings"
 
 	bzl "github.com/bazelbuild/buildtools/build"
+	"github.com/bazelbuild/rules_go/go/tools/gazelle/config"
 	"github.com/bazelbuild/rules_go/go/tools/gazelle/generator"
 	"github.com/bazelbuild/rules_go/go/tools/gazelle/merger"
-	"github.com/bazelbuild/rules_go/go/tools/gazelle/rules"
 	"github.com/bazelbuild/rules_go/go/tools/gazelle/wspace"
 )
 
@@ -43,54 +43,25 @@ var (
 	mode          = flag.String("mode", "fix", "print: prints all of the updated BUILD files\n\tfix: rewrites all of the BUILD files in place\n\tdiff: computes the rewrite but then just does a diff")
 )
 
-var externalResolverFromName = map[string]rules.ExternalResolver{
-	"external": rules.External,
-	"vendored": rules.Vendored,
-}
+type emitFunc func(*config.Config, *bzl.File) error
 
-var modeFromName = map[string]func(*bzl.File) error{
+var modeFromName = map[string]emitFunc{
 	"print": printFile,
 	"fix":   fixFile,
 	"diff":  diffFile,
 }
 
-func getBuildFileName() string {
-	validBuildFileNames := validBuildFileNameSlice()
-	if len(validBuildFileNames) == 0 {
-		log.Fatal("No valid build file names specified")
-	}
-
-	return validBuildFileNames[0]
-}
-
-func validBuildFileNameSlice() []string {
-	return strings.Split(*buildFileName, ",")
-}
-
-func isValidBuildFileName(buildFileName string) bool {
-	for _, bfn := range validBuildFileNameSlice() {
-		if buildFileName == bfn {
-			return true
-		}
-	}
-	return false
-}
-
-func run(dirs []string, buildTags map[string]bool, emit func(*bzl.File) error, external rules.ExternalResolver) {
-	g, err := generator.New(*repoRoot, *goPrefix, getBuildFileName(), buildTags, external)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, d := range dirs {
+func run(c *config.Config, emit emitFunc) {
+	g := generator.New(c)
+	for _, d := range c.Dirs {
 		files := g.Generate(d)
 		for _, f := range files {
-			f.Path = filepath.Join(*repoRoot, f.Path)
-			existingFilePath, err := findBuildFile(filepath.Dir(f.Path))
+			f.Path = filepath.Join(c.RepoRoot, f.Path)
+			existingFilePath, err := findBuildFile(c, filepath.Dir(f.Path))
 			if os.IsNotExist(err) {
 				// No existing file, so write a new one
 				bzl.Rewrite(f, nil) // have buildifier 'format' our rules.
-				if err := emit(f); err != nil {
+				if err := emit(c, f); err != nil {
 					log.Print(err)
 				}
 				continue
@@ -105,7 +76,7 @@ func run(dirs []string, buildTags map[string]bool, emit func(*bzl.File) error, e
 				continue
 			}
 			bzl.Rewrite(f, nil) // have buildifier 'format' our rules.
-			if err := emit(f); err != nil {
+			if err := emit(c, f); err != nil {
 				log.Print(err)
 			}
 		}
@@ -144,23 +115,7 @@ func main() {
 	flag.Usage = usage
 	flag.Parse()
 
-	if *repoRoot == "" {
-		var err error
-		if *repoRoot, err = repo(flag.Args()); err != nil {
-			log.Fatal(err)
-		}
-	}
-	if *goPrefix == "" {
-		var err error
-		if *goPrefix, err = loadGoPrefix(*repoRoot); err != nil {
-			if !os.IsNotExist(err) {
-				log.Fatal(err)
-			}
-			log.Fatalf("-go_prefix not set and no root BUILD file found")
-		}
-	}
-
-	genericTags, err := parseBuildTags(*buildTags)
+	c, err := newConfiguration()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -170,22 +125,82 @@ func main() {
 		log.Fatalf("unrecognized mode %s", *mode)
 	}
 
-	er, ok := externalResolverFromName[*external]
-	if !ok {
-		log.Fatalf("unrecognized external resolver %s", *external)
-	}
-
-	args := flag.Args()
-	if len(args) == 0 {
-		args = append(args, ".")
-	}
-
-	run(args, genericTags, emit, er)
+	run(c, emit)
 }
 
-func findBuildFile(repo string) (string, error) {
-	for _, base := range validBuildFileNameSlice() {
-		p := filepath.Join(repo, base)
+func newConfiguration() (*config.Config, error) {
+	var c config.Config
+	var err error
+
+	c.Dirs = flag.Args()
+	if len(c.Dirs) == 0 {
+		c.Dirs = []string{"."}
+	}
+	for i := range c.Dirs {
+		c.Dirs[i], err = filepath.Abs(c.Dirs[i])
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if *repoRoot != "" {
+		c.RepoRoot = *repoRoot
+	} else if len(c.Dirs) == 1 {
+		c.RepoRoot, err = wspace.Find(c.Dirs[0])
+		if err != nil {
+			return nil, fmt.Errorf("-repo_root not specified, and WORKSPACE cannot be found: %v", err)
+		}
+	} else {
+		cwd, err := filepath.Abs(".")
+		if err != nil {
+			return nil, err
+		}
+		c.RepoRoot, err = wspace.Find(cwd)
+		if err != nil {
+			return nil, fmt.Errorf("-repo_root not specified, and WORKSPACE cannot be found: %v", err)
+		}
+	}
+
+	for _, dir := range c.Dirs {
+		if !isDescendingDir(dir, c.RepoRoot) {
+			return nil, fmt.Errorf("dir %q is not a subdirectory of repo root %q", dir, c.RepoRoot)
+		}
+	}
+
+	c.ValidBuildFileNames = strings.Split(*buildFileName, ",")
+	if len(c.ValidBuildFileNames) == 0 {
+		return nil, fmt.Errorf("no valid build file names specified")
+	}
+
+	c.GenericTags = make(config.BuildTags)
+	for _, t := range strings.Split(*buildTags, ",") {
+		if strings.HasPrefix(t, "!") {
+			return nil, fmt.Errorf("build tags can't be negated: %s", t)
+		}
+		c.GenericTags[t] = true
+	}
+	c.Platforms = config.DefaultPlatformTags
+	c.PreprocessTags()
+
+	c.GoPrefix = *goPrefix
+	if c.GoPrefix == "" {
+		c.GoPrefix, err = loadGoPrefix(&c)
+		if err != nil {
+			return nil, fmt.Errorf("-go_prefix not set and not root BUILD file found")
+		}
+	}
+
+	c.DepMode, err = config.DependencyModeFromString(*external)
+	if err != nil {
+		return nil, err
+	}
+
+	return &c, err
+}
+
+func findBuildFile(c *config.Config, dir string) (string, error) {
+	for _, base := range c.ValidBuildFileNames {
+		p := filepath.Join(dir, base)
 		fi, err := os.Stat(p)
 		if err == nil {
 			if fi.Mode().IsRegular() {
@@ -200,8 +215,8 @@ func findBuildFile(repo string) (string, error) {
 	return "", os.ErrNotExist
 }
 
-func loadGoPrefix(repo string) (string, error) {
-	p, err := findBuildFile(repo)
+func loadGoPrefix(c *config.Config) (string, error) {
+	p, err := findBuildFile(c, c.RepoRoot)
 	if err != nil {
 		return "", err
 	}
@@ -237,28 +252,9 @@ func loadGoPrefix(repo string) (string, error) {
 	return "", errors.New("-go_prefix not set, and no go_prefix in root BUILD file")
 }
 
-func repo(args []string) (string, error) {
-	if len(args) == 1 {
-		return args[0], nil
+func isDescendingDir(dir, root string) bool {
+	if dir == root {
+		return true
 	}
-	cwd, err := os.Getwd()
-	if err != nil {
-		return "", err
-	}
-	r, err := wspace.Find(cwd)
-	if err != nil {
-		return "", fmt.Errorf("-repo_root not specified, and WORKSPACE cannot be found: %v", err)
-	}
-	return r, nil
-}
-
-func parseBuildTags(buildTags string) (map[string]bool, error) {
-	tags := make(map[string]bool)
-	for _, t := range strings.Split(buildTags, ",") {
-		if strings.HasPrefix(t, "!") {
-			return nil, fmt.Errorf("build tags can't be negated: %s", t)
-		}
-		tags[t] = true
-	}
-	return tags, nil
+	return strings.HasPrefix(dir, fmt.Sprintf("%s%c", root, filepath.Separator))
 }

go/tools/gazelle/gazelle/print.go
@@ -19,9 +19,10 @@ import (
 	"os"
 
 	bzl "github.com/bazelbuild/buildtools/build"
+	"github.com/bazelbuild/rules_go/go/tools/gazelle/config"
 )
 
-func printFile(f *bzl.File) error {
+func printFile(c *config.Config, f *bzl.File) error {
 	_, err := os.Stdout.Write(bzl.Format(f))
 	return err
 }

commit 0abc2779c18094d7f7330bebfba4ced108fd2345

Restore -external flag value to vendored
Commit cafe930223abe6f5faa0561d40435b52fbea77b9 inadvertently changed
the flag value from vendored to vendor.

go/tools/gazelle/config/config.go
@@ -123,7 +123,7 @@ func DependencyModeFromString(s string) (DependencyMode, error) {
 	switch s {
 	case "external":
 		return ExternalMode, nil
-	case "vendor":
+	case "vendored":
 		return VendorMode, nil
 	default:
 		return 0, fmt.Errorf("unrecognized dependency mode: %q", s)