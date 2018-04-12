bazelbuild/rules_go

commit 7c8af872cab81d70ccece4059f5dcada88f5d4d2

Include import paths in coverage file names (#1407)
Related #1150

go/tools/builders/generate_test_main.go
@@ -138,7 +138,7 @@ func coverRegisterAll() testing.Cover {
 	//{{$p.Import}}
 {{range $v := $p.Files}}
 	{{$var := printf "%s.%s" $p.Name $v.Var}}
-	coverRegisterFile(&coverage, {{$v.File | printf "%q"}}, {{$var}}.Count[:], {{$var}}.Pos[:], {{$var}}.NumStmt[:])
+	coverRegisterFile(&coverage, "{{printf "%s/%s" $p.Import $v.File}}", {{$var}}.Count[:], {{$var}}.Pos[:], {{$var}}.NumStmt[:])
 {{end}}
 {{end}}
 	return coverage

commit 2dc01e4d099a43f79d7cdd5a6233ee1bebf8d17a

coverage: Only print importpath and base name in log file
This fixes a filename stutter bug introduced in #1407.

Related #1406
Related #1150

go/tools/builders/generate_test_main.go
@@ -37,6 +37,10 @@ type CoverFile struct {
 	Var  string
 }
 
+func (f *CoverFile) Base() string {
+	return filepath.Base(f.File)
+}
+
 type CoverPackage struct {
 	Name   string
 	Import string
@@ -138,7 +142,7 @@ func coverRegisterAll() testing.Cover {
 	//{{$p.Import}}
 {{range $v := $p.Files}}
 	{{$var := printf "%s.%s" $p.Name $v.Var}}
-	coverRegisterFile(&coverage, "{{printf "%s/%s" $p.Import $v.File}}", {{$var}}.Count[:], {{$var}}.Pos[:], {{$var}}.NumStmt[:])
+	coverRegisterFile(&coverage, "{{printf "%s/%s" $p.Import $v.Base}}", {{$var}}.Count[:], {{$var}}.Pos[:], {{$var}}.NumStmt[:])
 {{end}}
 {{end}}
 	return coverage