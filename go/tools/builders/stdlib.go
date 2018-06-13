bazelbuild/rules_go

commit 5a2ee838ff7e4836cf59d4728a9febab4920e4a6

Refactor how environment and arguments are passed to builders (#1465)
Builders now receive inputs through three well-defined channels:

* Builder arguments (before --) are interpreted directly by
  builders. These arguments may or may not be passed on to underlying
  tools.
* Tool arguments (after --) are passed on to underlying
  tools. Builders may add additional arguments before or after these.
* Environment variables are passed on to underlying tools. Builders
  will usually not modify these.

Fixes #1456

go/tools/builders/stdlib.go
@@ -18,57 +18,53 @@ package main
 import (
 	"flag"
 	"fmt"
+	"go/build"
 	"log"
 	"os"
-	"os/exec"
 	"strings"
 )
-
-func install_stdlib(goenv *GoEnv, target string, args []string) error {
-	if goenv.tags != "" {
-		args = append(args, "-tags", goenv.tags)
-	}
-	args = append(args, target)
-	cmd := exec.Command(goenv.Go, args...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Env = goenv.Env()
-	if err := cmd.Run(); err != nil {
-		return fmt.Errorf("error running go install %s: %v", target, err)
-	}
-	return nil
-}
 
 func run(args []string) error {
 	// process the args
 	flags := flag.NewFlagSet("stdlib", flag.ExitOnError)
 	goenv := envFlags(flags)
-	filter_buildid := flags.String("filter_buildid", "", "Path to filter_buildid tool")
+	filterBuildid := flags.String("filter_buildid", "", "Path to filter_buildid tool")
 	out := flags.String("out", "", "Path to output go root")
 	race := flags.Bool("race", false, "Build in race mode")
+	shared := flags.Bool("shared", false, "Build in shared mode")
 	if err := flags.Parse(args); err != nil {
 		return err
 	}
-	if err := goenv.update(); err != nil {
+	if err := goenv.checkFlags(); err != nil {
 		return err
 	}
-	goroot := goenv.rootPath
+	goroot := os.Getenv("GOROOT")
+	if goroot == "" {
+		return fmt.Errorf("GOROOT not set")
+	}
 	output := abs(*out)
+
 	// Link in the bare minimum needed to the new GOROOT
 	if err := replicate(goroot, output, replicatePaths("src", "pkg/tool", "pkg/include")); err != nil {
 		return err
 	}
+
 	// Now switch to the newly created GOROOT
-	goenv.rootPath = output
-	// Run the commands needed to build the std library in the right mode
-	installArgs := []string{"install", "-toolexec", abs(*filter_buildid)}
+	os.Setenv("GOROOT", output)
+
+	// Make sure we have an absolute path to the C compiler.
+	// TODO(#1357): also take absolute paths of includes and other paths in flags.
+	os.Setenv("CC", abs(os.Getenv("CC")))
+
+	// Build the commands needed to build the std library in the right mode
+	installArgs := []string{"install", "-toolexec", abs(*filterBuildid)}
 	gcflags := []string{}
 	ldflags := []string{"-trimpath", abs(".")}
 	asmflags := []string{"-trimpath", abs(".")}
 	if *race {
 		installArgs = append(installArgs, "-race")
 	}
-	if goenv.shared {
+	if *shared {
 		gcflags = append(gcflags, "-shared")
 		ldflags = append(ldflags, "-shared")
 		asmflags = append(asmflags, "-shared")
@@ -78,23 +74,27 @@ func run(args []string) error {
 	// and its dependencies, rather than just the package itself. This was the
 	// default behavior before Go 1.10.
 	allSlug := ""
-	if goReleaseTags["go1.10"] {
-		allSlug = "all="
+	for _, t := range build.Default.ReleaseTags {
+		if t == "go1.10" {
+			allSlug = "all="
+			break
+		}
 	}
 	installArgs = append(installArgs, "-gcflags="+allSlug+strings.Join(gcflags, " "))
 	installArgs = append(installArgs, "-ldflags="+allSlug+strings.Join(ldflags, " "))
 	installArgs = append(installArgs, "-asmflags="+allSlug+strings.Join(asmflags, " "))
 
-	if err := install_stdlib(goenv, "std", installArgs); err != nil {
-		return err
-	}
-	if err := install_stdlib(goenv, "runtime/cgo", installArgs); err != nil {
-		return err
+	for _, target := range []string{"std", "runtime/cgo"} {
+		if err := goenv.runGoCommand(append(installArgs, target)); err != nil {
+			return err
+		}
 	}
 	return nil
 }
 
 func main() {
+	log.SetFlags(0)
+	log.SetPrefix("GoStdlib: ")
 	if err := run(os.Args[1:]); err != nil {
 		log.Fatal(err)
 	}

go/private/actions/asm.bzl
@@ -28,23 +28,24 @@ def emit_asm(go,
   if source == None: fail("source is a required parameter")
 
   out_obj = go.declare_file(go, path=source.basename[:-2], ext=".o")
-  includes = sets.union(
-      [go.stdlib.root_file.dirname + "/pkg/include"],
-      [f.dirname for f in hdrs])
   inputs = hdrs + go.stdlib.files + [source]
 
-  asm_args = go.args(go)
-  asm_args.add(["-o", out_obj, "-trimpath", "."])
+  args = go.args(go)
+  args.add([source, "--"])
+  includes = ([go.stdlib.root_file.dirname + "/pkg/include"] +
+              [f.dirname for f in hdrs])
+  # TODO(#1463): use uniquify=True when available.
+  includes = sorted({i: None for i in includes}.keys())
+  args.add(includes, before_each="-I")
+  args.add(["-trimpath", ".", "-o", out_obj])
   if go.mode.link == LINKMODE_C_SHARED:
-    asm_args.add("-shared")
-  asm_args.add(includes, before_each="-I")
-  asm_args.add(source.path)
+    args.add("-shared")
   go.actions.run(
       inputs = inputs,
       outputs = [out_obj],
-      mnemonic = "GoAsmCompile",
+      mnemonic = "GoAsm",
       executable = go.builders.asm,
-      arguments = [asm_args],
+      arguments = [args],
       env = go.env,
   )
   return out_obj

go/private/actions/compile.bzl
@@ -46,46 +46,45 @@ def emit_compile(go,
     if archives:  fail("compile does not accept deps in bootstrap mode")
     return _bootstrap_compile(go, sources, out_lib, gc_goopts)
 
-  # Add in any mode specific behaviours
-  if go.mode.race:
-    gc_goopts = gc_goopts + ["-race"]
-  if go.mode.msan:
-    gc_goopts = gc_goopts + ["-msan"]
-  if go.mode.link == LINKMODE_C_SHARED:
-    gc_goopts = gc_goopts + ["-shared"]
+  inputs = (sources + [go.package_list] +
+            [archive.data.file for archive in archives] +
+            go.stdlib.files)
 
+  builder_args = go.args(go)
+  builder_args.add(sources, before_each="-src")
+  builder_args.add(archives, before_each="-dep", map_fn=_importpath)
+  builder_args.add(archives, before_each="-importmap", map_fn=_importmap)
+  builder_args.add(["-o", out_lib])
+  builder_args.add(["-package_list", go.package_list])
+  if testfilter:
+    builder_args.add(["-testfilter", testfilter])
+
+  tool_args = go.actions.args()
+  tool_args.add(archives, before_each="-I", map_fn=_searchpath)
+  tool_args.add(["-trimpath", ".", "-I", "."])
   #TODO: Check if we really need this expand make variables in here
   #TODO: If we really do then it needs to be moved all the way back out to the rule
   gc_goopts = [go._ctx.expand_make_variables("gc_goopts", f, {}) for f in gc_goopts]
-  inputs = sets.union(sources, [go.package_list])
-
-  inputs = sets.union(inputs, [archive.data.file for archive in archives])
-  inputs = sets.union(inputs, go.stdlib.files)
-
-  args = go.args(go)
-  args.add(["-package_list", go.package_list])
-  args.add([s.path for s in sources], before_each="-src")
-  args.add(archives, before_each="-dep", map_fn=_importpath)
-  args.add(archives, before_each="-I", map_fn=_searchpath)
-  args.add(archives, before_each="-importmap", map_fn=_importmap)
-  args.add(["-o", out_lib, "-trimpath", ".", "-I", "."])
-  if testfilter:
-    args.add(["--testfilter", testfilter])
-  args.add(["--"])
+  tool_args.add(gc_goopts)
+  if go.mode.race:
+    tool_args.add("-race")
+  if go.mode.msan:
+    tool_args.add("-msan")
+  if go.mode.link == LINKMODE_C_SHARED:
+    tool_args.add("-shared")
   if importpath:
-    args.add(["-p", importpath])
-  args.add(gc_goopts)
-  args.add(go.toolchain.flags.compile)
+    tool_args.add(["-p", importpath])
   if go.mode.debug:
-    args.add(["-N", "-l"])
+    tool_args.add(["-N", "-l"])
   if go.mode.link in [LINKMODE_PLUGIN]:
-    args.add(["-dynlink"])
+    tool_args.add(["-dynlink"])
+  tool_args.add(go.toolchain.flags.compile)
   go.actions.run(
       inputs = inputs,
       outputs = [out_lib],
       mnemonic = "GoCompile",
       executable = go.builders.compile,
-      arguments = [args],
+      arguments = [builder_args, "--", tool_args],
       env = go.env,
   )
 

go/private/actions/link.bzl
@@ -42,34 +42,23 @@ def emit_link(go,
   config_strip = len(go._ctx.configuration.bin_dir.path) + 1
   pkg_depth = executable.dirname[config_strip:].count('/') + 1
 
-  ld = None
-  extldflags = []
-  if go.cgo_tools:
-    ld = go.cgo_tools.compiler_executable
-    extldflags.extend(go.cgo_tools.options)
-  # TODO(#1456): set rpaths in CgoCodegen instead of here.
-  origin = "@loader_path/" if go.mode.goos == "darwin" else "$ORIGIN/"
-  base_rpath = origin + "../" * pkg_depth
-  extldflags.append("-Wl,-rpath," + base_rpath)
-
+  extldflags = list(go.cgo_tools.linker_options)
   gc_linkopts, extldflags = _extract_extldflags(gc_linkopts, extldflags)
-
-  args = go.args(go)
+  builder_args = go.args(go)
+  tool_args = go.actions.args()
 
   # Add in any mode specific behaviours
-  link_external = False
+  extld = go.cgo_tools.compiler_executable
+  tool_args.add(["-extld", extld])
   if go.mode.race:
-    gc_linkopts.append("-race")
+    tool_args.add("-race")
   if go.mode.msan:
-    gc_linkopts.append("-msan")
+    tool_args.add("-msan")
   if go.mode.static:
     extldflags.append("-static")
   if go.mode.link != LINKMODE_NORMAL:
-    args.add(["-buildmode", go.mode.link])
-    link_external = True
-
-  if link_external:
-    gc_linkopts.extend(["-linkmode", "external"])
+    builder_args.add(["-buildmode", go.mode.link])
+    tool_args.add(["-linkmode", "external"])
 
   # Build the set of transitive dependencies. Currently, we tolerate multiple
   # archives with the same importmap (though this will be an error in the
@@ -83,57 +72,64 @@ def emit_link(go,
               if not any([d.importmap == t.importmap for t in test_archives])]
   dep_args.extend(["{}={}={}".format(d.label, d.importmap, d.file.path)
                    for d in test_archives])
-  args.add(dep_args, before_each="-dep")
+  builder_args.add(dep_args, before_each="-dep")
 
+  # Build a list of rpaths for dynamic libraries we need to find.
+  # rpaths are relative paths from the binary to directories where libraries
+  # are stored. Binaries that require these will only work when installed in
+  # the bazel execroot. Most binaries are only dynamically linked against
+  # system libraries though.
+  # TODO: there has to be a better way to work out the rpath.
+  config_strip = len(go._ctx.configuration.bin_dir.path) + 1
+  pkg_depth = executable.dirname[config_strip:].count('/') + 1
+  origin = "@loader_path/" if go.mode.goos == "darwin" else "$ORIGIN/"
+  base_rpath = origin + "../" * pkg_depth
   cgo_dynamic_deps = [d for d in archive.cgo_deps
                       if any([d.basename.endswith(ext) for ext in SHARED_LIB_EXTENSIONS])]
+  cgo_rpaths = []
   for d in cgo_dynamic_deps:
     short_dir = d.dirname[len(d.root.path)+len("/"):]
-    extldflags.append("-Wl,-rpath,{}/{}".format(base_rpath, short_dir))
+    cgo_rpaths.append("-Wl,-rpath,{}/{}".format(base_rpath, short_dir))
+  cgo_rpaths = sorted({p: None for p in cgo_rpaths}.keys())
+  extldflags.extend(cgo_rpaths)
 
   # Process x_defs, either adding them directly to linker options, or
   # saving them to process through stamping support.
   stamp_x_defs = False
   for k, v in archive.x_defs.items():
     if v.startswith("{") and v.endswith("}"):
-      args.add(["-Xstamp", "%s=%s" % (k, v[1:-1])])
+      builder_args.add(["-Xstamp", "%s=%s" % (k, v[1:-1])])
       stamp_x_defs = True
     else:
-      args.add(["-Xdef", "%s=%s" % (k, v)])
+      tool_args.add(["-X", "%s=%s" % (k, v)])
 
   # Stamping support
   stamp_inputs = []
   if stamp_x_defs or linkstamp:
     stamp_inputs = [info_file, version_file]
-    args.add(stamp_inputs, before_each="-stamp")
+    builder_args.add(stamp_inputs, before_each="-stamp")
     # linkstamp option support: read workspace status files,
     # converting "KEY value" lines to "-X $linkstamp.KEY=value" arguments
     # to the go linker.
     if linkstamp:
-      args.add(["-linkstamp", linkstamp])
+      builder_args.add(["-linkstamp", linkstamp])
 
-  args.add(extldflags, before_each = "-ld_flag")
-  args.add(["-out", executable])
-
-  args.add(["--"])
-  args.add(gc_linkopts)
-  args.add(go.toolchain.flags.link)
+  builder_args.add(["-o", executable])
+  builder_args.add(["-main", archive.data.file])
+  tool_args.add(gc_linkopts)
+  tool_args.add(go.toolchain.flags.link)
   if go.mode.strip:
-    args.add(["-w"])
-
-  if ld:
-    args.add([
-        "-extld", ld,
-    ])
-
-  args.add(archive.data.file)
+    tool_args.add("-w")
+  if extldflags:
+    tool_args.add(["-extldflags", " ".join(extldflags)])
+  
   go.actions.run(
       inputs = sets.union(archive.libs, archive.cgo_deps,
                 go.crosstool, stamp_inputs, go.stdlib.files),
       outputs = [executable],
       mnemonic = "GoLink",
       executable = go.builders.link,
-      arguments = [args],
+      arguments = [builder_args, "--", tool_args],
       env = go.env,
   )
 
@@ -156,12 +152,14 @@ def _extract_extldflags(gc_linkopts, extldflags):
 
   Args:
     gc_linkopts: a list of flags passed in through the gc_linkopts attributes.
-      ctx.expand_make_variables should have already been applied.
+      ctx.expand_make_variables should have already been applied. -extldflags
+      may appear multiple times in this list.
     extldflags: a list of flags to be passed to the external linker.
 
   Return:
     A tuple containing the filtered gc_linkopts with external flags removed,
-    and a combined list of external flags.
+    and a combined list of external flags. Each string in the returned
+    extldflags list may contain multiple flags, separated by whitespace.
   """
   filtered_gc_linkopts = []
   is_extldflags = False

go/private/actions/pack.bzl
@@ -22,24 +22,19 @@ def emit_pack(go,
   if in_lib == None: fail("in_lib is a required parameter")
   if out_lib == None: fail("out_lib is a required parameter")
 
-  inputs = [in_lib] + go.stdlib.files
+  inputs = [in_lib] + go.stdlib.files + objects + archives
 
-  arguments = go.args(go)
-  arguments.add([
-      "-in", in_lib,
-      "-out", out_lib,
-  ])
-  inputs.extend(objects)
-  arguments.add(objects, before_each="-obj")
-
-  inputs.extend(archives)
-  arguments.add(archives, before_each="-arc")
+  args = go.args(go)
+  args.add(["-in", in_lib])
+  args.add(["-out", out_lib])
+  args.add(objects, before_each="-obj")
+  args.add(archives, before_each="-arc")
 
   go.actions.run(
       inputs = inputs,
       outputs = [out_lib],
       mnemonic = "GoPack",
       executable = go.builders.pack,
-      arguments = [arguments],
+      arguments = [args],
       env = go.env,
   )

go/private/context.bzl
@@ -82,29 +82,9 @@ def _declare_directory(go, path="", ext="", name = ""):
 
 def _new_args(go):
   args = go.actions.args()
-  if go.stdlib:
-    root_file = go.stdlib.root_file
-  else:
-    root_file = go.package_list
-  args.add([
-      "-go", go.go,
-      "-root_file", root_file,
-      "-goos", go.mode.goos,
-      "-goarch", go.mode.goarch,
-      "-cgo=" + ("0" if go.mode.pure else "1"),
-  ])
-  if len(go.tags) > 0:
-    args.add("-tags")
-    args.add(go.tags, join_with = ",")
-  if go.cgo_tools:
-    args.add([
-      "-compiler_path", go.cgo_tools.compiler_path,
-      "-cc", go.cgo_tools.compiler_executable,
-    ])
-    args.add(go.cgo_tools.compiler_options, before_each = "-c_flag")
-    args.add(go.cgo_tools.compiler_options, before_each = "-cxx_flag")
-    args.add(go.cgo_tools.compiler_options, before_each = "-cpp_flag")
-    args.add(go.cgo_tools.linker_options, before_each = "-ld_flag")
+  args.add(["-go", go.go])
+  if go.tags:
+    args.add(["-tags", ",".join(go.tags)])
   return args
 
 def _new_library(go, name=None, importpath=None, resolver=None, importable=True, testfilter=None, **kwargs):
@@ -245,6 +225,19 @@ def go_context(ctx, attr=None):
   stdlib = getattr(attr, "_stdlib", None)
   if stdlib:
     stdlib = get_source(stdlib).stdlib
+    goroot = stdlib.root_file.dirname
+  else:
+    goroot = root
+
+  env = dict(context_data.env)
+  env.update({
+      "GOARCH": mode.goarch,
+      "GOOS": mode.goos,
+      "GOROOT": goroot,
+      "GOROOT_FINAL": "GOROOT",
+      "CGO_ENABLED": "0" if mode.pure else "1",
+      "PATH": context_data.cgo_tools.compiler_path,
+  })
 
   importpath, pathtype = _infer_importpath(ctx)
   return GoContext(
@@ -266,7 +259,7 @@ def go_context(ctx, attr=None):
       cgo_tools = context_data.cgo_tools,
       builders = builders,
       coverdata = coverdata if have_cover else None,
-      env = context_data.env,
+      env = env,
       tags = context_data.tags,
       # Action generators
       archive = toolchain.actions.archive,

go/private/rules/cgo.bzl
@@ -108,11 +108,17 @@ def _select_archives(libs):
     fail("cc_library(s) did not produce any files")
   return outs
 
+def _include_unique(opts, flag, include, seen):
+  if include in seen:
+    return
+  seen[include] = True
+  opts.extend([flag, include])
+
 def _cgo_codegen_impl(ctx):
   go = go_context(ctx)
   if not go.cgo_tools:
     fail("Go toolchain does not support cgo")
-  linkopts = ctx.attr.linkopts[:]
+  linkopts = go.cgo_tools.linker_options + ctx.attr.linkopts
   cppopts = go.cgo_tools.compiler_options + ctx.attr.cppopts
   copts = go.cgo_tools.c_options + ctx.attr.copts
   deps = depset([], order="topological")
@@ -122,9 +128,9 @@ def _cgo_codegen_impl(ctx):
   cgo_types = go.declare_file(go, path="_cgo_gotypes.go")
   out_dir = cgo_main.dirname
 
-  cc = go.cgo_tools.compiler_executable
-  args = go.args(go)
-  args.add(["-cc", str(cc), "-objdir", out_dir])
+  builder_args = go.args(go)     # interpreted by builder
+  tool_args = ctx.actions.args() # interpreted by cgo
+  cc_args = ctx.actions.args()   # interpreted by C compiler
 
   c_outs = [cgo_export_h, cgo_export_c]
   cxx_outs = [cgo_export_h]
@@ -132,9 +138,13 @@ def _cgo_codegen_impl(ctx):
   transformed_go_outs = []
   gen_go_outs = [cgo_types]
 
+  seen_includes = {}
+  seen_quote_includes = {}
+  seen_system_includes = {}
+
   source = split_srcs(ctx.files.srcs)
-  for src in source.headers:
-    cppopts.extend(['-iquote', src.dirname])
+  for hdr in source.headers:
+    _include_unique(cppopts, "-iquote", hdr.dirname, seen_quote_includes)
   stems = {}
   for src in source.go:
     mangled_stem, src_ext = _mangle(src, stems)
@@ -142,27 +152,29 @@ def _cgo_codegen_impl(ctx):
     gen_c_file = go.declare_file(go, path=mangled_stem + ".cgo2.c")
     transformed_go_outs.append(gen_go_file)
     c_outs.append(gen_c_file)
-    args.add(["-src", gen_go_file.path + "=" + src.path])
+    builder_args.add(["-src", gen_go_file.path + "=" + src.path])
   for src in source.asm:
     mangled_stem, src_ext = _mangle(src, stems)
     gen_file = go.declare_file(go, path=mangled_stem + ".cgo1."+src_ext)
     transformed_go_outs.append(gen_file)
-    args.add(["-src", gen_file.path + "=" + src.path])
+    builder_args.add(["-src", gen_file.path + "=" + src.path])
   for src in source.c:
     mangled_stem, src_ext = _mangle(src, stems)
     gen_file = go.declare_file(go, path=mangled_stem + ".cgo1."+src_ext)
     c_outs.append(gen_file)
-    args.add(["-src", gen_file.path + "=" + src.path])
+    builder_args.add(["-src", gen_file.path + "=" + src.path])
   for src in source.cxx:
     mangled_stem, src_ext = _mangle(src, stems)
     gen_file = go.declare_file(go, path=mangled_stem + ".cgo1."+src_ext)
     cxx_outs.append(gen_file)
-    args.add(["-src", gen_file.path + "=" + src.path])
+    builder_args.add(["-src", gen_file.path + "=" + src.path])
   for src in source.objc:
     mangled_stem, src_ext = _mangle(src, stems)
     gen_file = go.declare_file(go, path=mangled_stem + ".cgo1."+src_ext)
     objc_outs.append(gen_file)
-    args.add(["-src", gen_file.path + "=" + src.path])
+    builder_args.add(["-src", gen_file.path + "=" + src.path])
+
+  tool_args.add(["-objdir", out_dir])
 
   inputs = sets.union(ctx.files.srcs, go.crosstool, go.stdlib.files,
                       *[d.cc.transitive_headers for d in ctx.attr.deps])
@@ -172,39 +184,47 @@ def _cgo_codegen_impl(ctx):
     runfiles = runfiles.merge(d.data_runfiles)
     cppopts.extend(['-D' + define for define in d.cc.defines])
     for inc in d.cc.include_directories:
-      cppopts.extend(['-I', inc])
+      _include_unique(cppopts, "-I", inc, seen_includes)
     for inc in d.cc.quote_include_directories:
-      cppopts.extend(['-iquote', inc])
+      _include_unique(cppopts, "-iquote", inc, seen_quote_includes)
     for inc in d.cc.system_include_directories:
-      cppopts.extend(['-isystem', inc])
+      _include_unique(cppopts, "-isystem", inc, seen_system_includes)
     for lib in as_iterable(d.cc.libs):
       # If both static and dynamic variants are available, Bazel will only give
-      # us the static variant. We'll get on file for each transitive dependency,
+      # us the static variant. We'll get one file for each transitive dependency,
       # so the same file may appear more than once.
-      # TODO(#1456): deduplicate flags, get rid of -L, and set rpaths.
       if (lib.basename.startswith("lib") and
           any([lib.basename.endswith(ext) for ext in SHARED_LIB_EXTENSIONS])):
+        # If the loader would be able to find the library using rpaths,
+        # use -L and -l instead of hard coding the path to the library in
+        # the binary. This gives users more flexibility. The linker will add
+        # rpaths later. We can't add them here because they are relative to
+        # the binary location, and we don't know where that is.
         libname = lib.basename[len("lib"):lib.basename.rindex(".")]
         linkopts.extend(['-L', lib.dirname, '-l', libname])
       else:
         linkopts.append(lib.path)
     linkopts.extend(d.cc.link_flags)
 
-  args.add(linkopts, before_each="-ld_flag")
+  # cgo writes CGO_LDFLAGS to _cgo_import.go in the form of pragmas which get
+  # compiled into .a files. The linker finds these and passes them to the
+  # external linker.
+  # TODO(jayconrod): do we need to set this here, or only in _cgo_import?
+  # go build does it here.
+  env = go.env
+  env["CC"] = go.cgo_tools.compiler_executable
+  env["CGO_LDFLAGS"] = " ".join(linkopts)
 
-  # The first -- below is to stop the cgo from processing args, the
-  # second is an actual arg to forward to the underlying go tool
-  args.add(["--", "--"])
-  args.add(cppopts)
-  args.add(copts)
+  cc_args.add(cppopts + copts)
+
   ctx.actions.run(
-      inputs = inputs + go.crosstool,
+      inputs = inputs,
       outputs = c_outs + cxx_outs + objc_outs + gen_go_outs + transformed_go_outs + [cgo_main],
       mnemonic = "CGoCodeGen",
       progress_message = "CGoCodeGen %s" % ctx.label,
       executable = go.builders.cgo,
-      arguments = [args],
-      env = go.env,
+      arguments = [builder_args, "--", tool_args, "--", cc_args],
+      env = env,
   )
 
   return [
@@ -247,9 +267,11 @@ def _cgo_import_impl(ctx):
   out = go.declare_file(go, path="_cgo_import.go")
   args = go.args(go)
   args.add([
+      "-import",
+      "-src", ctx.files.sample_go_srcs[0],
+      "--",  # stop builder from processing args
       "-dynout", out,
       "-dynimport", ctx.file.cgo_o,
-      "-src", ctx.files.sample_go_srcs[0],
   ])
   ctx.actions.run(
       inputs = [

go/private/rules/stdlib.bzl
@@ -42,13 +42,20 @@ def _stdlib_library_to_source(go, attr, source, merge):
     args.add("-shared")
   args.add(["-filter_buildid", filter_buildid.path])
   go.actions.write(root_file, "")
+  env = go.env
+  env.update({
+      "CC": go.cgo_tools.compiler_executable,
+      "CGO_CPPFLAGS": " ".join(go.cgo_tools.compiler_options),
+      "CGO_CFLAGS": " ".join(go.cgo_tools.c_options),
+      "CGO_LDFLAGS": " ".join(go.cgo_tools.linker_options),
+  })
   go.actions.run(
       inputs = go.sdk_files + go.sdk_tools + go.crosstool + [filter_buildid, go.package_list, root_file],
       outputs = [pkg],
       mnemonic = "GoStdlib",
       executable = attr._stdlib_builder.files.to_list()[0],
       arguments = [args],
-      env = go.env,
+      env = env,
   )
   source["stdlib"] = GoStdLib(
       root_file = root_file,

commit 18093e31917c6f097aa3fb46cc5687be1f49d9a4

Use absolute path to the compiler dir in stdlib builder
Needed to fix a regression after
https://github.com/bazelbuild/rules_go/pull/1465.

go/tools/builders/stdlib.go
@@ -21,6 +21,7 @@ import (
 	"go/build"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -55,6 +56,13 @@ func run(args []string) error {
 	// Make sure we have an absolute path to the C compiler.
 	// TODO(#1357): also take absolute paths of includes and other paths in flags.
 	os.Setenv("CC", abs(os.Getenv("CC")))
+
+	// Ensure paths are absolute.
+	absPaths := []string{}
+	for _, path := range filepath.SplitList(os.Getenv("PATH")) {
+		absPaths = append(absPaths, abs(path))
+	}
+	os.Setenv("PATH", strings.Join(absPaths, string(os.PathListSeparator)))
 
 	// Build the commands needed to build the std library in the right mode
 	installArgs := goenv.goCmd("install", "-toolexec", abs(*filterBuildid))