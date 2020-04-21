4e0cfaf342b70faf96353e4a666a9d51157a7149
Jay Conrod <jayconrod@google.com>
2020-04-21 17:15:58 -0400

    go: remove mode prefixes and import paths from output file paths
    Fixes #2448

--- tests/core/output_groups/compilation_outputs_test.go
+++ tests/core/output_groups/compilation_outputs_test.go
@@ -3,40 +3,55 @@ package output_groups
 import (
 	"os"
 	"path/filepath"
+	"runtime"
+	"sort"
 	"strings"
 	"testing"
+
+	"github.com/bazelbuild/rules_go/go/tools/bazel"
 )
 
 func TestCompilationOutputs(t *testing.T) {
-	expectedFiles := map[string]bool{
-		"compilation_outputs_test": true, // test binary; not relevant
-
-		"lib%/lib.a":                          true, // :lib archive
-		"lib_test%/lib.a":                     true, // :lib_test archive
-		"bin%/tests/core/output_groups/bin.a": true, // :bin archive
+	runfiles, err := bazel.ListRunfiles()
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+	exe := ""
+	if runtime.GOOS == "windows" {
+		exe = ".exe"
+	}
+	expectedFiles := map[string]bool{
+		"compilation_outputs_test" + exe: true, // test binary; not relevant
+
+		"lib.a":               true, // :lib archive
+		"lib_test.internal.a": true, // :lib_test archive
+		"bin.a":               true, // :bin archive
+	}
+	for _, rf := range runfiles {
+		info, err := os.Stat(rf.Path)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
 		if info.IsDir() {
-			return nil
+			continue
 		}
-		// Remove first directory of file (e.g. linux_amd64_stripped)
-		if firstSlash := strings.Index(path, "/"); firstSlash >= 0 {
-			path = path[firstSlash+1:]
-		}
-		if expectedFiles[path] {
-			delete(expectedFiles, path)
+
+		base := strings.TrimSuffix(filepath.Base(rf.Path), ".exe")
+		if expectedFiles[base] {
+			delete(expectedFiles, base)
 		} else {
-			t.Errorf("Runfiles contains an unexpected file: %s", path)
+			t.Errorf("unexpected runfile: %s", rf.Path)
 		}
-		return nil
-	})
+	}
 
 	if len(expectedFiles) != 0 {
 		var missingFiles []string
 		for path := range expectedFiles {
 			missingFiles = append(missingFiles, path)
 		}
-		t.Errorf("Could find expected files: %v", missingFiles)
+		sort.Strings(missingFiles)
+		t.Errorf("Could find expected files: %s", strings.Join(missingFiles, " "))
 	}
 }
