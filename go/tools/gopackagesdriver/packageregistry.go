b08da87a1c1919cdb28e0f6775d58469cf49a377
Steeve Morin <steeve@zen.ly>
2021-04-04 14:01:44 +0200

    go/tools/gopackagesdriver: don't index by package ID or files
    The indexing by ID was never used except for iterating on all packages.
    The file index isn't used either since the whole graph is dumped anyway.
    
    Signed-off-by: Steeve Morin <steeve@zen.ly>

--- go/tools/gopackagesdriver/packageregistry.go
+++ go/tools/gopackagesdriver/packageregistry.go
@@ -1,16 +1,12 @@
 package main
 
 type PackageRegistry struct {
-	packagesByID         map[string]*FlatPackage
 	packagesByImportPath map[string]*FlatPackage
-	packagesByFile       map[string]*FlatPackage
 }
 
 func NewPackageRegistry(pkgs ...*FlatPackage) *PackageRegistry {
 	pr := &PackageRegistry{
-		packagesByID:         map[string]*FlatPackage{},
 		packagesByImportPath: map[string]*FlatPackage{},
-		packagesByFile:       map[string]*FlatPackage{},
 	}
 	pr.Add(pkgs...)
 	return pr
@@ -18,55 +14,39 @@ func NewPackageRegistry(pkgs ...*FlatPackage) *PackageRegistry {
 
 func (pr *PackageRegistry) Add(pkgs ...*FlatPackage) *PackageRegistry {
 	for _, pkg := range pkgs {
-		pr.packagesByID[pkg.ID] = pkg
 		pr.packagesByImportPath[pkg.PkgPath] = pkg
-		for _, f := range pkg.GoFiles {
-			pr.packagesByFile[f] = pkg
-		}
 	}
 	return pr
-}
-
-func (pr *PackageRegistry) FromPkgID(pkgPath string) *FlatPackage {
-	return pr.packagesByImportPath[pkgPath]
 }
 
 func (pr *PackageRegistry) FromPkgPath(pkgPath string) *FlatPackage {
 	return pr.packagesByImportPath[pkgPath]
 }
 
-func (pr *PackageRegistry) FromFile(filePath string) *FlatPackage {
-	return pr.packagesByFile[filePath]
-}
-
 func (pr *PackageRegistry) Remove(pkgs ...*FlatPackage) *PackageRegistry {
 	for _, pkg := range pkgs {
-		delete(pr.packagesByID, pkg.ID)
 		delete(pr.packagesByImportPath, pkg.PkgPath)
-		for _, f := range pkg.GoFiles {
-			delete(pr.packagesByFile, f)
-		}
 	}
 	return pr
 }
 
 func (pr *PackageRegistry) ToList() []*FlatPackage {
-	pkgs := make([]*FlatPackage, 0, len(pr.packagesByID))
-	for _, pkg := range pr.packagesByID {
+	pkgs := make([]*FlatPackage, 0, len(pr.packagesByImportPath))
+	for _, pkg := range pr.packagesByImportPath {
 		pkgs = append(pkgs, pkg)
 	}
 	return pkgs
 }
 
 func (pr *PackageRegistry) ResolvePaths(prf PathResolverFunc) error {
-	for _, pkg := range pr.packagesByID {
+	for _, pkg := range pr.packagesByImportPath {
 		pkg.ResolvePaths(prf)
 	}
 	return nil
 }
 
 func (pr *PackageRegistry) ResolveImports() error {
-	for _, pkg := range pr.packagesByID {
+	for _, pkg := range pr.packagesByImportPath {
 		pkg.ResolveImports(func(importPath string) *FlatPackage {
 			return pr.FromPkgPath(importPath)
 		})
