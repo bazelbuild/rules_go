777c96103042a51de0999dd2baa4dea49532f563
jamy <jamy@uber.com>
2022-01-20 21:28:34 +0000

    fixup! separate tests between success and fail

--- go/tools/releaser/upgradedep_test.go
+++ go/tools/releaser/upgradedep_test.go
@@ -7,11 +7,10 @@ import (
 	bzl "github.com/bazelbuild/buildtools/build"
 )
 
-func TestPatchItemParser(t *testing.T) {
+func TestPatchItemParser_Success(t *testing.T) {
 	tests := []struct {
 		expression []byte
 		result     string
-		error      string
 	}{
 		{
 			expression: []byte(`# releaser:patch-cmd gazelle -repo_root . -go_prefix golang.org/x/tools -go_naming_convention import_alias
@@ -33,11 +32,34 @@ func TestPatchItemParser(t *testing.T) {
 			Label("@io_bazel_rules_go//third_party:org_golang_x_tools-gazelle.patch")`),
 			result: "@io_bazel_rules_go//third_party:org_golang_x_tools-gazelle.patch",
 		},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.expression), func(t *testing.T) {
+			patchExpr, err := bzl.Parse("repos.bzl", tt.expression)
+			if err != nil {
+				t.Fatalf(err.Error())
+			}
+
+			patchLabelStr, _, err := parsePatchesItem(patchExpr.Stmt[0])
+			if err != nil {
+				t.Errorf("unexpected error while parsing expression: %q", err.Error())
+			} else if patchLabelStr != tt.result {
+				t.Errorf("expected result %q, but got result %q instead", tt.result, patchLabelStr)
+			}
+		})
+	}
+}
+
+func TestPatchItemParser_Error(t *testing.T) {
+	tests := []struct {
+		expression []byte
+		error      string
+	}{
 		{
 			expression: []byte(`# releaser:patch-cmd gazelle -repo_root . -go_prefix golang.org/x/tools -go_naming_convention import_alias
 			NotLabel("//third_party:org_golang_x_tools-gazelle.patch")`),
-			result: "",
-			error:  `invalid patch function: "NotLabel"`,
+			error: `invalid patch function: "NotLabel"`,
 		},
 		{
 			expression: []byte(`# releaser:patch-cmd gazelle -repo_root . -go_prefix golang.org/x/tools -go_naming_convention import_alias
@@ -69,20 +91,11 @@ func TestPatchItemParser(t *testing.T) {
 			}
 
 			patchLabelStr, _, err := parsePatchesItem(patchExpr.Stmt[0])
-			if err != nil && err.Error() != tt.error {
-				if tt.error != "" {
-					t.Errorf("expected error %q, but got error %q instead", tt.error, err.Error())
-				} else {
-					t.Errorf("unexpected error while parsing expression: %s", err.Error())
-				}
-			}
 
-			if err == nil && patchLabelStr != tt.result {
-				if tt.error != "" {
-					t.Errorf("expected error %q, but got result %q instead", tt.error, patchLabelStr)
-				} else {
-					t.Errorf("expected result %q, but got result %q instead", tt.result, patchLabelStr)
-				}
+			if err == nil {
+				t.Errorf("expected error %q, but got result %q instead", tt.error, patchLabelStr)
+			} else if err.Error() != tt.error {
+				t.Errorf("expected error %q, but got error %q instead", tt.error, err.Error())
 			}
 		})
 	}
