51da04ce66033b00f78c0850bb85b5903e050970
Talha Pathan <tpathan@twitter.com>
2022-07-28 13:42:47 +0100

    Add integration test

--- tests/core/go_test/env_inherit_test.go
+++ tests/core/go_test/env_inherit_test.go
@@ -17,11 +17,45 @@ package env_inherit_test
 import (
 	"os"
 	"testing"
+
+	"github.com/bazelbuild/rules_go/go/tools/bazel_testing"
 )
 
-func TestEnv(t *testing.T) {
+func TestMain(m *testing.M) {
+	bazel_testing.TestMain(m, bazel_testing.Args{
+		Main: `
+-- src/BUILD.bazel --
+load("@io_bazel_rules_go//go:def.bzl", "go_test")
+go_test(
+    name = "main",
+	srcs = ["env_inherit.go"],
+	env_inherit = ["INHERITEDVAR"],
+)
+-- src/env_inherit.go --
+package env_inherit_test
+
+import (
+	"os"
+	"testing"
+)
+
+func TestInherit(t *testing.T) {
 	v := os.Getenv("INHERITEDVAR")
 	if v != "b" {
 		t.Fatalf("INHERITEDVAR was not equal to b")
+	}
+}
+`,
+
+		SetUp: func() error {
+			os.Setenv("INHERITEDVAR", "b")
+			return nil
+		},
+	})
+}
+
+func TestInheritedEnvVar(t *testing.T) {
+	if err := bazel_testing.RunBazel("test", "//src:main"); err != nil {
+		t.Fatal(err)
 	}
 }
