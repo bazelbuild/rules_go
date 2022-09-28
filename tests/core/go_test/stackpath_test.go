3ae0ba8895ad8f1a5d7f27d24230f3143aa8c8f5
Artem V. Navrotskiy <bozaro@yandex.ru>
2022-09-28 16:00:10 +0300

    Rewrite `stackpath_..._test`

--- tests/core/go_test/stackpath_test.go
+++ tests/core/go_test/stackpath_test.go
@@ -1,37 +1,51 @@
 package stackpath
 
 import (
+	"fmt"
 	"runtime/debug"
+	"stackpath_default_lib"
 	"stackpath_lib"
 	"strings"
 	"testing"
 )
 
+var expectedLibFile string
+var expectedTestFile string
+
 func TestStackPath(t *testing.T) {
 	stack := stackpath_lib.Wrap(func() string {
 		return string(debug.Stack())
 	})
-	// Stack example:
-	//
-	//	goroutine 7 [running]:
-	//	runtime/debug.Stack()
-	//		GOROOT/src/runtime/debug/stack.go:24 +0x65
-	//	tests/core/go_test/stackpath_test.TestStackPath.func1(...)
-	//		bar/stackpath_test.go:12
-	//	stackpath_lib.Wrap(...)
-	//		foo/stackpath_lib.go:4
-	//	tests/core/go_test/stackpath_test.TestStackPath(0xc00009a9c0)
-	//		bar/stackpath_test.go:11 +0x35
-	//	testing.tRunner(0xc00009a9c0, 0x575e50)
-	//		GOROOT/src/testing/testing.go:1439 +0x102
-	//	created by testing.(*T).Run
-	//		GOROOT/src/testing/testing.go:1486 +0x35f
-	for _, expected := range []string{
-		"\tbar/stackpath_test.go:",
-		"\tfoo/stackpath_lib.go:",
+	for _, expectedFile := range []string{
+		expectedLibFile,
+		expectedTestFile,
 	} {
-		if !strings.Contains(stack, expected) {
-			t.Fatalf("Stacktrace does not contains substring %q", expected)
+		if expectedFile == "" {
+			t.Fatalf("Unexpected empty value. Looks like x_defs missed.")
+		}
+
+		expectedSubstring := fmt.Sprintf("\n\t%s:", expectedFile)
+		if !strings.Contains(stack, expectedSubstring) {
+			t.Fatalf("Stacktrace does not contains substring %q: %s", expectedSubstring, stack)
+		}
+	}
+}
+
+func TestStackDefaultPath(t *testing.T) {
+	stack := stackpath_default_lib.Wrap(func() string {
+		return string(debug.Stack())
+	})
+	for _, expectedFile := range []string{
+		"tests/core/go_test/stackpath_default_lib.go",
+		expectedTestFile,
+	} {
+		if expectedFile == "" {
+			t.Fatalf("Unexpected empty value. Looks like x_defs missed.")
+		}
+
+		expectedSubstring := fmt.Sprintf("\n\t%s:", expectedFile)
+		if !strings.Contains(stack, expectedSubstring) {
+			t.Fatalf("Stacktrace does not contains substring %q: %s", expectedSubstring, stack)
 		}
 	}
 }
